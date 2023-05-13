a900851606516be7acd75c23322172046dbe9782
Teo <teoirado@gmail.com>
2023-05-13 19:13:06 -0300

    bug: Corrigindo um bug em pokemonColors

--- src/components/pokemonModal.tsx
+++ src/components/pokemonModal.tsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { API_URL } from "../utils/constants";
-import pokemonColors from "@/utils/pokemonColor";
 
 export default function PokemonModal() {
   const { id, isOpen, close } = useModalStore();
@@ -124,3 +123,29 @@ export default function PokemonModal() {
     </>
   );
 }
+
+type PokemonColorProps = {
+  [key: string]: string;
+};
+const pokemonColors: PokemonColorProps = {
+  normal: "bg-gray-400",
+  fighting: "bg-red-700",
+  flying: "bg-blue-400",
+  poison: "bg-purple-700",
+  ground: "bg-yellow-700",
+  rock: "bg-yellow-400",
+  bug: "bg-green-400",
+  ghost: "bg-purple-400",
+  steel: "bg-gray-500",
+  fire: "bg-red-400",
+  water: "bg-blue-700",
+  grass: "bg-green-700",
+  electric: "bg-yellow-400",
+  psychic: "bg-pink-400",
+  ice: "bg-blue-200",
+  dragon: "bg-red-600",
+  dark: "bg-gray-800",
+  fairy: "bg-pink-200",
+  unknown: "bg-gray-400",
+  shadow: "bg-gray-400",
+};
